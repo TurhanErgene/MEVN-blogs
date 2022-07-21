e54c3834f9cf15dc93f306a3a77abfa8ef529f4c
Turhan <turhan-ergene@hotmail.com>
2022-07-21 14:58:33 +0200

    create post from form

--- client/src/components/PostForm.vue
+++ client/src/components/PostForm.vue
@@ -1,51 +1,56 @@
 <template>
   <div class="container">
-    <div class="field">
-      <label class="label">Name</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Text input" />
+    <form @submit.prevent="submitForm">
+      <div class="field mt-3">
+        <label class="label">Title</label>
+        <div class="control">
+          <input
+            v-model="post.title"
+            class="input"
+            type="text"
+            placeholder="Title of your post"
+          />
+        </div>
       </div>
-    </div>
 
-    <div class="field">
-      <label class="label">Username</label>
-      <div class="control has-icons-left has-icons-right">
-        <input
-          class="input is-success"
-          type="text"
-          placeholder="Text input"
-          value="bulma"
-        />
-        <span class="icon is-small is-left">
-          <i class="fas fa-user"></i>
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-check"></i>
-        </span>
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea
+            v-model="post.content"
+            class="textarea"
+            placeholder="Wassup?"
+          ></textarea>
+        </div>
       </div>
-      <p class="help is-success">This username is available</p>
-    </div>
+      <div class="field">
+        <label class="label">Creator</label>
+        <div class="control">
+          <input
+            v-model="post.creator"
+            class="input"
+            type="text"
+            placeholder="Who are you?"
+          />
+        </div>
+      </div>
 
-    <div class="field">
-      <label class="label">Message</label>
-      <div class="control">
-        <textarea class="textarea" placeholder="Textarea"></textarea>
+      <div class="field is-grouped">
+        <div class="control">
+          <button type="submit" class="button is-primary">Submit</button>
+        </div>
+        <div class="control">
+          <router-link to="/" class="button"> Cancel </router-link>
+        </div>
       </div>
-    </div>
-
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-link">Submit</button>
-      </div>
-      <div class="control">
-        <button class="button is-link is-light">Cancel</button>
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["post", "submitForm"],
+};
 </script>
 
 <style>
--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,13 +1,54 @@
 <template>
-  <post-form />
+  <post-form :post="post" :submitForm="createPost" />
   <!-- buyuk harflerle baslayinca bu sekilde de kullanilabiliyor -->
 </template>
 
 <script>
 import PostForm from "../components/PostForm";
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+
 export default {
   components: {
     PostForm,
+  },
+  setup() {
+    const API_URL = "http://localhost:5000/posts";
+    const router = useRouter();
+    const post = reactive({
+      title: "",
+      content: "",
+      creator: "",
+    });
+
+    async function createPost() {
+      try {
+        const response = await fetch(API_URL, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            title: post.title,
+            content: post.content,
+            creator: post.creator,
+          }),
+        });
+        
+        //unnecessary
+        const json = await response.json();
+
+        router.push({
+          name: "Home",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return {
+      post,
+      createPost,
+    };
   },
 };
 </script>
