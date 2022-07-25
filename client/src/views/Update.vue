4323dff650aea31b8a42d902cc853bc9bd480986
Turhan <turhan-ergene@hotmail.com>
2022-07-25 03:36:43 +0200

    views/update => getPost

--- client/src/views/Update.vue
+++ client/src/views/Update.vue
@@ -3,7 +3,33 @@
 </template>
 
 <script>
-export default {};
+import PostForm from "../components/PostForm";
+import { useRouter, useRoute } from "vue-router";
+import { ref, onMounted } from "vue"; //composition api
+
+export default {
+  components: {
+    PostForm,
+  },
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+
+    const API_URL = "http://localhost:5000/posts";
+
+    const post = ref({
+      title: "",
+      content: "",
+      creator: "",
+    });
+
+    async function getPost() {
+      const { id } = route.params;
+      console.log(id);
+    }
+    return getPost();
+  },
+};
 </script>
 
 <style>
